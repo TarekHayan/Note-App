2eae077c503c4bc7bda42d913f52466fe171b208
TarekHayan <tarekhayan77@gmail.com>
2025-09-18 23:00:55 +0300

    fixed keyboard hight

--- lib/widgets/custom_model_buttom_sheet.dart
+++ lib/widgets/custom_model_buttom_sheet.dart
@@ -24,7 +24,11 @@ class CustomModelButtomSheet extends StatelessWidget {
           return AbsorbPointer(
             absorbing: state is AddNoteLoading ? true : false,
             child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16),
+              padding: EdgeInsets.only(
+                right: 16,
+                left: 16,
+                bottom: MediaQuery.of(context).viewInsets.bottom,
+              ),
               child: const SingleChildScrollView(child: CheakAvaildNote()),
             ),
           );

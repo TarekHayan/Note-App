714b998baa2c09a3003a4e7eaa1c4b3d2865d20d
TarekHayan <tarekhayan77@gmail.com>
2025-09-18 12:37:01 +0300

    link logic with Ui

--- lib/widgets/custom_model_buttom_sheet.dart
+++ lib/widgets/custom_model_buttom_sheet.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:note_app/cubits/cubit/add_note_cubit.dart';
 import 'package:note_app/widgets/cheak_availb_note.dart';
 
 class CustomModelButtomSheet extends StatelessWidget {
@@ -6,9 +9,27 @@ class CustomModelButtomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
+    return Padding(
       padding: EdgeInsets.symmetric(horizontal: 16),
-      child: SingleChildScrollView(child: CheakAvaildNote()),
+      child: SingleChildScrollView(
+        child: BlocConsumer<AddNoteCubit, AddNoteState>(
+          listener: (context, state) {
+            if (state is AddNotefaliar) {
+              print("faild ${state.errorMassege}");
+            }
+
+            if (state is AddNoteSuccess) {
+              Navigator.pop(context);
+            }
+          },
+          builder: (context, state) {
+            return ModalProgressHUD(
+              inAsyncCall: state is AddNoteLoading ? true : false,
+              child: const CheakAvaildNote(),
+            );
+          },
+        ),
+      ),
     );
   }
 }
